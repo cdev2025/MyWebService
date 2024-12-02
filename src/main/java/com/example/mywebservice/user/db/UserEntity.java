bf0202fab3dec40680e93c099fbc315cbc3ba715
cdev2025 <creativedev2025@gmail.com>
2024-12-02 15:01:40 +0900

    [2024-12-02] user ㅕãService, WebSecurityConfig, Controller 추가

--- src/main/java/com/example/mywebservice/user/db/UserEntity.java
+++ src/main/java/com/example/mywebservice/user/db/UserEntity.java
@@ -3,6 +3,7 @@ package com.example.mywebservice.user.db;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@Data
 @Entity
 @Table(name = "users" )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
